5f9ecb9b43d6a2b4c81eaf04c5321bddf62c3de6
Diana Corado <cor191025@uvg.edu.gt>
2020-06-03 22:00:10 -0600

    login 1/2

---
+++ app/src/main/java/com/example/foodforme/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.foodforme.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/foodforme/login/loginFragment.kt
@@ -0,0 +1,40 @@
+package com.example.foodforme.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+
+import com.example.foodforme.R
+import com.example.foodforme.databinding.LoginFragmentBinding
+
+class loginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = loginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
+
