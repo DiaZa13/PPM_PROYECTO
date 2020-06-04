805dcc619e021c602012014a10f614dade4b27a6
Diana Corado <cor191025@uvg.edu.gt>
2020-06-03 22:24:28 -0600

    New account

---
+++ app/src/main/java/com/example/foodforme/newAccount/NewAccountViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.foodforme.newAccount
+
+import androidx.lifecycle.ViewModel
+
+class NewAccountViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/foodforme/newAccount/newAccountFragment.kt
@@ -0,0 +1,38 @@
+package com.example.foodforme.newAccount
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+
+import com.example.foodforme.R
+import com.example.foodforme.databinding.NewAccountFragmentBinding
+
+class newAccountFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = newAccountFragment()
+    }
+
+    private lateinit var viewModel: NewAccountViewModel
+    private lateinit var binding: NewAccountFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.new_account_fragment, container, false)
+
+        return binding.root    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NewAccountViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
