41ec1d4bc0ecd2e8a95659a83a489e394ebe663b
Pablo Méndez <pablomendez778@gmail.com>
2020-06-09 08:58:58 -0600

    Added Rate Restaurant Fragment

---
+++ app/src/main/java/com/example/foodforme/rateRestaurant/RateRestaurantFragment.kt
@@ -0,0 +1,27 @@
+package com.example.foodforme.rateRestaurant
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodforme.R
+import com.example.foodforme.databinding.FragmentFilterBinding
+import com.example.foodforme.databinding.FragmentRateRestaurantBinding
+import com.example.foodforme.filterFragment.FilterViewModel
+
+class RateRestaurantFragment : Fragment() {
+    private lateinit var binding: FragmentRateRestaurantBinding
+    private lateinit var viewModel: FilterViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Muestro el Fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_rate_restaurant, container, false)
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
+}
