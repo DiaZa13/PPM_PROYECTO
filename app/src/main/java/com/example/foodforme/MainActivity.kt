c16c7116730beb9e0617c6a1df086072bff4b328
Pablo Méndez <pablomendez778@gmail.com>
2020-06-09 14:01:10 -0600

    Traté de cambiar el appbarLayout para que no apareciera tanto

--- app/src/main/java/com/example/foodforme/MainActivity.kt
+++ app/src/main/java/com/example/foodforme/MainActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.foodforme.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -20,23 +23,43 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
         drawerLayout = binding.drawerLayout
 
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        val topLevelDestinations = setOf(
+            R.id.aboutFragment,
+            R.id.showOfertasFragment,
+            R.id.filterFragment
+        )
 
-        NavigationUI.setupWithNavController(binding.navView,navController)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+            .setDrawerLayout(drawerLayout)
+            .build()
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _: Bundle? ->
+            if (nd.id in topLevelDestinations) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
 
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController,drawerLayout)
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
     }
 
 
