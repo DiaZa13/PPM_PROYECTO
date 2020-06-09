4f0e20243be07b8f0314be85c9201478bbcdefc9
Pablo Méndez <pablomendez778@gmail.com>
2020-06-09 10:47:30 -0600

    Added Restaurant info Fragment

--- app/src/main/java/com/example/foodforme/showRestaurantes/ShowRestaurantsViewModel.kt
+++ app/src/main/java/com/example/foodforme/showRestaurantes/ShowRestaurantsViewModel.kt
@@ -14,13 +14,13 @@ class ShowRestaurantsViewModel: ViewModel(){
 
     init{
         _datos.value = listOf(
-            Restaurant("Restaurante A", "1111","Dirección A", 5f),
-            Restaurant("Restaurante B", "2222","Dirección B", 4.5f),
-            Restaurant("Restaurante C", "3333","Dirección C", 4f),
-            Restaurant("Restaurante D", "4444","Dirección D", 3.5f),
-            Restaurant("Restaurante E", "5555","Dirección E", 3f),
-            Restaurant("Restaurante F", "6666","Dirección F", 3.2f),
-            Restaurant("Restaurante G", "7777","Dirección G", 1f)
+            Restaurant("Restaurante A", "1111","Dirección A", 5f, "Taco"),
+            Restaurant("Restaurante B", "2222","Dirección B", 4.5f, "Hamburguesas"),
+            Restaurant("Restaurante C", "3333","Dirección C", 4f, "Pollo"),
+            Restaurant("Restaurante D", "4444","Dirección D", 3.5f, "Taco"),
+            Restaurant("Restaurante E", "5555","Dirección E", 3f, "Pizza"),
+            Restaurant("Restaurante F", "6666","Dirección F", 3.2f, "Pizza"),
+            Restaurant("Restaurante G", "7777","Dirección G", 1f, "Taco" )
             )
     }
 
