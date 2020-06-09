380eea01b55b3c6453b58ef452afa88132bf8b79
Diana Corado <cor191025@uvg.edu.gt>
2020-06-09 14:17:07 -0600

    FIREBASE REALTIME DONEEEEEE

--- app/src/main/java/com/example/foodforme/data/Fb_restaurantes.kt
+++ app/src/main/java/com/example/foodforme/data/Fb_restaurantes.kt
@@ -2,12 +2,11 @@ package com.example.foodforme.data
 
 import java.io.Serializable
 
-// De seguro le vamos a agregar más cosas pero por ahora eso está bien
-data class Restaurant(
+class Fb_restaurantes (
     val id: String = "",
     val direction: String = "",
     val name: String = "",
     val phone: Int = 12345678,
     val rating: Int = 1,
     val type: String = ""
-)
+): Serializable
--- app/src/main/java/com/example/foodforme/showRestaurantes/ShowRestaurantsViewModel.kt
+++ app/src/main/java/com/example/foodforme/showRestaurantes/ShowRestaurantsViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.foodforme.data.Fb_restaurantes
 import com.example.foodforme.data.Restaurant
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -15,10 +16,13 @@ import com.google.firebase.ktx.Firebase
 class ShowRestaurantsViewModel: ViewModel(){
 
 
-    private val _datos = MutableLiveData<List<Restaurant>>()
-
-    val datos: LiveData<List<Restaurant>>
+    private val _datos = MutableLiveData<List<Fb_restaurantes>>()
+    val datos: LiveData<List<Fb_restaurantes>>
         get() = _datos
+
+    private val _prueba = MutableLiveData<List<Restaurant>>()
+    val prueba: LiveData<List<Restaurant>>
+        get() = _prueba
 
     init{
         val ref = FirebaseDatabase.getInstance().getReference("restaurantes")
@@ -30,7 +34,7 @@ class ShowRestaurantsViewModel: ViewModel(){
                         var restaurant = i.getValue(Restaurant::class.java)
                         if (restaurant != null) {
                             _datos.value = mutableListOf(
-                                Restaurant(restaurant.id,restaurant.direction,restaurant.name,restaurant.phone,restaurant.rating,restaurant.type)
+                                Fb_restaurantes(restaurant.id,restaurant.direction,restaurant.name,restaurant.phone,restaurant.rating,restaurant.type)
                             )
 
                         }
