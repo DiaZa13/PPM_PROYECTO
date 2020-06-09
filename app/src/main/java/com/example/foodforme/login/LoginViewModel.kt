887bdd78e49dec6806b55d0a4e694073749b26b8
Diana Corado <cor191025@uvg.edu.gt>
2020-06-08 23:37:44 -0600

    LogIn

--- app/src/main/java/com/example/foodforme/login/LoginViewModel.kt
+++ app/src/main/java/com/example/foodforme/login/LoginViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.foodforme.login
 
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginViewModel (): ViewModel() {
 
@@ -14,7 +16,23 @@ class LoginViewModel (): ViewModel() {
     val isUser: LiveData<Boolean>
         get() = _isUser
 
+    private val _error = MutableLiveData<Int>()
+    val error: LiveData<Int>
+        get() = _error
 
+    fun logIn(){
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(user.value!!, password.value!!)
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    _isUser.value = true
+                }else{
+                    _isUser.value = false
+                    Log.w("NewAccountViewModel", it.exception)
+                    
+                }
+            }
+    }
 
 
 }
