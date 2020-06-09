2d58f645976a4726a84d6e24e8df64df937ba3e3
Orlando Cabrera <orlando.osberto@gmail.com>
2020-06-09 13:52:25 -0600

    Cambiando algunas cosas para el firebase

--- app/src/main/java/com/example/foodforme/showRestaurantes/ShowRestaurantsViewModel.kt
+++ app/src/main/java/com/example/foodforme/showRestaurantes/ShowRestaurantsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.foodforme.data.Restaurant
+import com.example.foodforme.data.Restaurante
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -15,23 +16,24 @@ import com.google.firebase.ktx.Firebase
 class ShowRestaurantsViewModel: ViewModel(){
 
 
-    private val _datos = MutableLiveData<List<Restaurant>>()
+    private val _datos = MutableLiveData<List<Restaurante>>()
 
-    val datos: LiveData<List<Restaurant>>
+    val datos: LiveData<List<Restaurante>>
         get() = _datos
 
     init{
-        val ref = FirebaseDatabase.getInstance().getReference("restaurant")
+        val ref = FirebaseDatabase.getInstance().getReference("restaurantes")
         ref.addValueEventListener(object:ValueEventListener{
             @SuppressLint("LongLogTag")
             override fun onDataChange(p0: DataSnapshot) {
                 if (p0!!.exists()){
                     for(i in p0.children){
-                        var restaurant = i.getValue(Restaurant::class.java)
+                        var restaurant = i.getValue(Restaurante::class.java)
                         if (restaurant != null) {
                             _datos.value = mutableListOf(
-                                Restaurant(restaurant.id,restaurant.direction,restaurant.name,restaurant.phone,restaurant.rating,restaurant.type)
+                                restaurant
                             )
+                            Log.i("Restaurant",restaurant.id+ restaurant.direction+ restaurant.name )
 
                         }
                     }
