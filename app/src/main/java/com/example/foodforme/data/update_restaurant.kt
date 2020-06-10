d469bdfe15fd78c1b419fc4ed4021880dbb33c02
Diana Corado <cor191025@uvg.edu.gt>
2020-06-09 21:02:46 -0600

    I  played with your heart

---
+++ app/src/main/java/com/example/foodforme/data/update_restaurant.kt
@@ -0,0 +1,11 @@
+package com.example.foodforme.data
+
+import java.io.Serializable
+
+class update_restaurant (
+    val direction: String = "",
+    val name: String = "",
+    val phone: String = "",
+    val rating: Int = 1,
+    val type: String = ""
+): Serializable
--- app/src/main/java/com/example/foodforme/rateRestaurant/RateRestaurantFragment.kt
+++ app/src/main/java/com/example/foodforme/rateRestaurant/RateRestaurantFragment.kt
@@ -1,6 +1,7 @@
 package com.example.foodforme.rateRestaurant
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.foodforme.R
 import com.example.foodforme.data.Fb_restaurantes
+import com.example.foodforme.data.update_restaurant
 import com.example.foodforme.databinding.FragmentFilterBinding
 import com.example.foodforme.databinding.FragmentRateRestaurantBinding
 import com.example.foodforme.filterFragment.FilterViewModel
@@ -25,21 +27,23 @@ class RateRestaurantFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_rate_restaurant, container, false)
         binding.lifecycleOwner = this
 
-        val rate = binding.ratingBar.rating.toInt()
+        val rate = binding.ratingBar.numStars
 
         // Ahora sí Diana, seguía dormido
         val restaurante: Fb_restaurantes = arguments?.getSerializable("Restaurant") as Fb_restaurantes
 
-        var newRate = (restaurante.rating) + (rate/3) - 1
+        var newRate = (restaurante.rating) + ((rate/3) - 1)
         if(newRate < 0){
             newRate = 0
         }else if(newRate > 5){
             newRate = 5
         }
-        val ref = FirebaseDatabase.getInstance().getReference("restaurantes")
-
+        val ref = FirebaseDatabase.getInstance().reference
+        val newRestaurant = update_restaurant(restaurante.direction,restaurante.name,restaurante.phone,newRate,restaurante.type)
+        Log.i("RateRestaurantFragment",restaurante.id)
         binding.button.setOnClickListener{
-            ref.child(restaurante.id).child("rating").setValue(newRate)
+            ref.child("restaurantes").child(restaurante.name).setValue(newRestaurant)
+
             findNavController().navigateUp()
 
         }
