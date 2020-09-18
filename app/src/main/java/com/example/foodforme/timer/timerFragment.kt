eb6dbfe317b03032ade85181c5228f1aab43d314
Diana Corado <cor191025@uvg.edu.gt>
2020-09-17 23:07:55 -0600

    :)

--- app/src/main/java/com/example/foodforme/timer/timerFragment.kt
+++ app/src/main/java/com/example/foodforme/timer/timerFragment.kt
@@ -1,33 +1,33 @@
 package com.example.foodforme.timer
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
 import com.example.foodforme.R
+import com.example.foodforme.databinding.TimerFragmentBinding
+
 
 class timerFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = timerFragment()
-    }
-
     private lateinit var viewModel: TimerViewModel
+    private lateinit var binding: TimerFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.timer_fragment, container, false)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.timer_fragment, container, false)
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TimerViewModel::class.java)
-        // TODO: Use the ViewModel
+        val application = requireNotNull(this.activity).application
     }
+
 
 }
